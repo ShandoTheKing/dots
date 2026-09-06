57c59dba491849c7269be02c9337bbc4800b6ef1
Chamath Withana <chamathwith@protonmail.com>
2026-09-07 03:00:09 +0530

    Grub timeout is 1 with timeoutStyle as hidden

--- hosts/rhea/hardware-configuration.nix
+++ hosts/rhea/hardware-configuration.nix
@@ -32,7 +32,8 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.grub.efiSupport = true;
-  boot.loader.grub.timeout = 0;
+  boot.loader.grub.timeout = 1;
+  boot.loader.grub.timeoutStyle = "hidden";
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.extraEntries = ''
